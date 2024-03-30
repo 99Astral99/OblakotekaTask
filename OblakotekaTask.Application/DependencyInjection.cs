15276706337af8a2fd465f6093809e4aac86ca4c
Viktor <strik4462@gmail.com>
2024-03-30 15:41:38 +0300

    feat: added dependency injection class

---
+++ OblakotekaTask.Application/DependencyInjection.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using OblakotekaTask.Application.Implementations;
+using OblakotekaTask.Application.Interfaces;
+
+namespace OblakotekaTask.Application
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddApplication(this IServiceCollection services)
+        {
+            services.AddScoped<IProductService, ProductService>();
+            return services;
+        }
+    }
+}
