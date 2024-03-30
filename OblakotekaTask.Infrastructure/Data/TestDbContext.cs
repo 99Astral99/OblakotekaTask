e006b732e36165b1ae492d7d9d85306421405d43
Viktor <strik4462@gmail.com>
2024-03-30 15:51:07 +0300

    feat: added db context

---
+++ OblakotekaTask.Infrastructure/Data/TestDbContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OblakotekaTask.Application.Interfaces;
+using OblakotekaTask.Domain.Entities;
+
+namespace OblakotekaTask.Infrastructure.Data;
+
+public partial class TestDbContext : DbContext, ITestDbContext
+{
+    public TestDbContext()
+    {
+    }
+
+    public TestDbContext(DbContextOptions<TestDbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<EventLog> EventLogs { get; set; }
+
+    public virtual DbSet<Product> Products { get; set; }
+
+    public virtual DbSet<ProductVersion> ProductVersions { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(TestDbContext).Assembly);
+    }
+}
---
+++ OblakotekaTask.Infrastructure/Interfaces/ITestDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OblakotekaTask.Domain.Entities;
+
+namespace OblakotekaTask.Application.Interfaces
+{
+    public interface ITestDbContext
+    {
+        public DbSet<EventLog> EventLogs { get; set; }
+
+        public DbSet<Product> Products { get; set; }
+
+        public DbSet<ProductVersion> ProductVersions { get; set; }
+
+        Task<int> SaveChangesAsync(CancellationToken cancellationToken);
+    }
+}
