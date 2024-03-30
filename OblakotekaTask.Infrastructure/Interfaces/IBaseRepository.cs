e836ff2e5250466d76dfd71e27f7079f29876fde
Viktor <strik4462@gmail.com>
2024-03-30 15:51:39 +0300

    feat: added product repository

---
+++ OblakotekaTask.Infrastructure/Interfaces/IBaseRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace OblakotekaTask.Infrastructure.Interfaces
+{
+    public interface IBaseRepository<T>
+    {
+        Task Create(T entity, CancellationToken cancellationToken = default);
+        Task<T> Update(T entity, CancellationToken cancellationToken = default);
+        Task Delete(T entity, CancellationToken cancellationToken = default);
+        IQueryable<T> GetAll();
+        Task<T> GetById(Guid id);
+    }
+}
---
+++ OblakotekaTask.Infrastructure/Repositories/ProductRepository.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OblakotekaTask.Application.Interfaces;
+using OblakotekaTask.Domain.Entities;
+using OblakotekaTask.Infrastructure.Interfaces;
+
+namespace OblakotekaTask.Infrastructure.Repositories
+{
+    public class ProductRepository : IBaseRepository<Product>
+    {
+        private readonly ITestDbContext _context;
+
+        public ProductRepository(ITestDbContext _context) =>
+            this._context = _context;
+
+
+        public async Task Create(Product entity, CancellationToken cancellationToken = default)
+        {
+            await _context.Products.AddAsync(entity);
+            await _context.SaveChangesAsync(cancellationToken);
+        }
+
+        public async Task Delete(Product entity, CancellationToken cancellationToken = default)
+        {
+            _context.Products.Remove(entity);
+            await _context.SaveChangesAsync(cancellationToken);
+        }
+
+        public IQueryable<Product> GetAll() => _context.Products.AsQueryable().AsNoTracking();
+
+        public async Task<Product> GetById(Guid id)
+        {
+            var product = await _context.Products.FirstOrDefaultAsync(x => x.Id == id);
+            if (product == null)
+                throw new Exception();
+            return product;
+        }
+
+        public async Task<Product> Update(Product entity, CancellationToken cancellationToken = default)
+        {
+            _context.Products.Update(entity);
+            await _context.SaveChangesAsync(cancellationToken);
+
+            return entity;
+        }
+    }
+}
